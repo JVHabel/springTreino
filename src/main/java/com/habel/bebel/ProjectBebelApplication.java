20c806d7dc36f3818949213b6e5a7d7224119e6f
Habel <jvhabelspi@hotmail.com>
2020-05-22 19:21:51 -0300

    implementando cliente edereco

--- src/main/java/com/habel/bebel/ProjectBebelApplication.java
+++ src/main/java/com/habel/bebel/ProjectBebelApplication.java
@@ -9,16 +9,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.habel.bebel.entity.Categoria;
 import com.habel.bebel.entity.Cidade;
+import com.habel.bebel.entity.Cliente;
+import com.habel.bebel.entity.Endereco;
 import com.habel.bebel.entity.Estado;
 import com.habel.bebel.entity.Produto;
+import com.habel.bebel.entity.enems.TipoCliente;
 import com.habel.bebel.repositories.CategoriaRepository;
 import com.habel.bebel.repositories.CidadeRepository;
+import com.habel.bebel.repositories.ClienteRepository;
+import com.habel.bebel.repositories.EnderecoRepository;
 import com.habel.bebel.repositories.EstadoRepository;
 import com.habel.bebel.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class ProjectBebelApplication implements CommandLineRunner {
 	
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
 	@Autowired
@@ -65,6 +75,16 @@ public class ProjectBebelApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA ,"12314212");
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 		
 		
 	}
